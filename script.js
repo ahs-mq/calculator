2306db52b8335d752dcafaa012997a449995f2e0
ahs-mq <141071955+ahs-mq@users.noreply.github.com>
2025-02-04 19:45:01 +0400

    added basic math functions

---
+++ script.js
@@ -0,0 +1,42 @@
+let first = 0;
+let second = 0;
+let operator = '';
+let selection = document.getElementsByTagName('button');
+
+function add(x,y){
+    return x + y;
+}
+
+function sub(x,y){
+    return x - y;
+}
+
+function divide(x,y){
+    if ( y == 0){
+    return alert('You can\'t divide by zero!')
+    }else{
+    return x / y;
+    }
+   
+}
+
+function mult(x,y){
+    return x * y;
+}
+
+function operate(x,op,y){
+    first = x;
+    second = y;
+    operator = op;
+
+    if (op == '+'){
+        return add(x,y);
+    } else if (op == '-'){
+        return sub(x,y);
+    } else if (op == 'x' || op == '*'){
+        return mult(x,y);
+    } else if (op == '/'){
+        return divide(x,y);
+    }
+}
+
